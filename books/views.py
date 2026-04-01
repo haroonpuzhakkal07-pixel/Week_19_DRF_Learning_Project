2d8696d5235021921c3f5d7df3d789485c0c60da
haroonpuzhakkal07-pixel <haroonpuzhakkal07@gmail.com>
2026-04-01 13:46:51 +0530

    Added filtering, search, and ordering to API

--- books/views.py
+++ books/views.py
@@ -1,9 +1,18 @@
 from .models import Book
 from .serializers import BookSerializer
-from rest_framework import viewsets
+from rest_framework import viewsets, filters
+from django_filters.rest_framework import DjangoFilterBackend
 
 # Create your views here.
 
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+    # Exact Filtering
+    filterset_fields = ['author', 'published_year']
+    # Search (Partial Match)
+    search_fields = ['title', 'author']
+    # Ordering
+    ordering_fields = ['published_year', 'title']
